4241f5405e1a6c0329ae1eff6a23a8642e4e074c
931697478@qq.com <931697478@qq.com>
2018-03-01 17:25:43 +0800

    Initial commit 百度语音识别

---
+++ app/src/main/java/com/lawyee/myaudio/adapter/MainAdaapter.java
@@ -0,0 +1,50 @@
+package com.lawyee.myaudio.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andview.refreshview.recyclerview.BaseRecyclerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * All rights Reserved, Designed By www.lawyee.com
+ *
+ * @version V 1.0 xxxxxxxx
+ * @Title: MyAudio
+ * @Package com.lawyee.myaudio.adapter
+ * @Description: $todo$
+ * @author: YFL
+ * @date: 2018/3/1 14:39
+ * @verdescript 版本号 修改时间  修改人 修改的概要说明
+ * @Copyright: 2018 www.lawyee.com Inc. All rights reserved.
+ * 注意：本内容仅限于北京法意科技有限公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class MainAdaapter extends BaseRecyclerAdapter {
+
+    private Context mContext;
+    private ArrayList mDatas;
+
+
+    @Override
+    public RecyclerView.ViewHolder getViewHolder(View view) {
+        return null;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType, boolean isItem) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position, boolean isItem) {
+
+    }
+
+    @Override
+    public int getAdapterItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/lawyee/myaudio/ui/ActivityUiDialog.java
@@ -0,0 +1,15 @@
+package com.lawyee.myaudio.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lawyee.myaudio.R;
+
+public class ActivityUiDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_dialog);
+    }
+}
